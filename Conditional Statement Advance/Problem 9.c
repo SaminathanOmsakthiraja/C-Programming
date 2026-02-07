039d5d06579f56e004ffb67c8883dd89b5ae7089
SaminathanOmsakthiraja <omsaminathan@gmail.com>
2026-02-07 09:37:31 +0530

    Vehicle Speed Limit

---
+++ Conditional Statement Advance/Problem 9.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+    int n;
+    scanf("%d",&n);
+    if (n>80) {
+        printf("Speed Limit Exceeded");
+    }
+    else {
+        printf("Within Speed Limit");
+    }
+}
