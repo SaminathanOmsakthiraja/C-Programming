042b3c3f284c1a9f1b54eed8bb792685f4488784
SaminathanOmsakthiraja <omsaminathan@gmail.com>
2026-02-08 16:11:18 +0530

    Count total duplicate pairs in the packet list.

---
+++ For loop/Problme 15.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main() {
+    int size;
+    scanf("%d", &size);
+    int arr[size];
+    for (int i = 0; i < size; i++) {
+        scanf("%d", &arr[i]);
+    }
+    int count = 0;
+    for (int i = 0; i < size; i++) {
+        for (int j = i+1; j < size; j++) {
+            if (arr[i] == arr[j]) {
+                count++;
+            }
+        }
+    }
+    printf("%d",count);
+}
