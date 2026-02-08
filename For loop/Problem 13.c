5b0fcdce54a3c357b246c6b906e8fcd6234b11e4
SaminathanOmsakthiraja <omsaminathan@gmail.com>
2026-02-08 15:50:49 +0530

    Find the longest consecutive increasing subsequence length

---
+++ For loop/Problem 13.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main() {
+    int size;
+    scanf("%d", &size);
+    int arr[size];
+    for (int i = 0; i < size; i++) {
+        scanf("%d", &arr[i]);
+    }
+    int count = 1;
+    for (int i=0; i<size-1; i++) {
+        if (arr[i] < arr[i+1]) {
+            count++;
+        }
+        if (arr[i] > arr[i+1]) {
+            break;
+        }
+    }
+    printf("%d\n", count);
+}
