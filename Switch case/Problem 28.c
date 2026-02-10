cb27ce719d0e5fd7c8ca7e6670af1ae49e8780f1
SaminathanOmsakthiraja <omsaminathan@gmail.com>
2026-02-10 09:44:19 +0530

    College Hostel Fee

---
+++ Switch case/Problem 28.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main() {
+    int n;
+    scanf("%d",&n);
+    int amt=0;
+    switch(n) {
+        case 1:
+            printf("Hostel Fee Rs%d",80000);;break;
+        case 2:
+            printf("Hostel Fee Rs%d",60000);break;
+        case 3:
+            printf("Hostel Fee Rs%d",45000);
+    }
+}
