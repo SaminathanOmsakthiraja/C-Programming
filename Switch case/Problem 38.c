ef98cfd078f8af52076630f0fed0d30026814cba
SaminathanOmsakthiraja <omsaminathan@gmail.com>
2026-02-10 10:24:44 +0530

    Hospital Consultation Fee

---
+++ Switch case/Problem 38.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+    int n;
+    scanf("%d",&n);
+    switch(n) {
+        case 1:
+            printf("Consultation Fee Rs%d",500);;break;
+        case 2:
+            printf("Consultation Fee Rs%d",1200);break;
+        }
+}
