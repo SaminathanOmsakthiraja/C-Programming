972628a42b058c6695192ba14557c192be36d03f
SaminathanOmsakthiraja <omsaminathan@gmail.com>
2026-02-10 10:21:41 +0530

    Online Course Certificate Fee

---
+++ Switch case/Problem 37.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+    int n;
+    scanf("%d",&n);
+    switch(n) {
+        case 1:
+            printf("Certificate Fee Rs%d",0);;break;
+        case 2:
+            printf("Certificate Fee Rs%d",500);break;
+        }
+}
