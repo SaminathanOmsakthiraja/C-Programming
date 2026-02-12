41b8f74da2f25fc371f8c81cc34b1ce6606abfbf
OM.Saminathan <omsaminathan@gmail.com>
2026-02-12 13:29:05 +0530

    Calculate total patients and count overcrowded days (>100 patients)

---
+++ For loop Part 2/Problem 11.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main() {
+    int n;
+    scanf("%d",&n);
+    int sum=0;
+    int count=0;
+    for(int i=0;i<n;i++) {
+        int j;
+        scanf("%d",&j);
+        sum+=j;
+        if (j>100)
+            count++;
+    }
+    printf("Total Patients: %d\n",sum);
+    printf("Overcrowded Days: %d",count);
+}
