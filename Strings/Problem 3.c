31c924238ecafd600393eb2f1be0081145210776
OM.Saminathan <omsaminathan@gmail.com>
2026-02-17 15:59:49 +0530

    Email Username Extractor

---
+++ Strings/Problem 3.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main() {
+    char str[100];
+    fgets(str,sizeof(str),stdin);
+    char username[20];
+    for (int i = 0; str[i]!='@'; i++) {
+        username[i] = str[i];
+    }
+    printf("%s",username);
+}
