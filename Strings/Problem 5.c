b15755f747ff3d9c195d99f436b4d3f2b508377b
OM.Saminathan <omsaminathan@gmail.com>
2026-02-17 16:05:51 +0530

    Reverse a Customer Feedback

---
+++ Strings/Problem 5.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include <string.h>
+
+int main() {
+    char str[100];
+    fgets(str,sizeof(str),stdin);
+    int len=strlen(str);
+    char feed[len+1];
+    int j=0;
+    for (int i=len-1;i>=0;i--) {
+        feed[j++]=str[i];
+    }
+    printf("%s",feed);
+}
